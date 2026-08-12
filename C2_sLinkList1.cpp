6e69085e337c8b96cbd4c94c0f98805ebcf70ad7
btk123-sss <bunthern.kim9@gmail.com>
2026-08-12 10:10:04 +0700

    Auto-commit: 2026-08-12 10:10:04

---
+++ C2_sLinkList1.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+
+
+class StringNode
+{               // A node in a list of strings
+private:
+    string elem;                   // element value
+    StringNode *next;              // next item in the list
+    friend class StringLinkedList; // provide StringLinkedList access
+};
+
+class StringLinkedList
+{               // A linked list of strings
+public:
+    StringLinkedList();             // empty list constructor
+    ~StringLinkedList();             // destructor
+    bool empty() const;             // is list empty?
+    const string &front() const;    // get front element
+    void addFront(const string &e); // add to front of list
+    void removeFront();             // remove front item list
+    void print() const;             // print all list elements
+private:
+    StringNode *head; // pointer to the head of list
+};
+
+
+// constructor
+StringLinkedList::StringLinkedList() 
+    : head(NULL) {}
+
+// destructor
+StringLinkedList::~StringLinkedList() 
+    { while (!empty()) removeFront(); }
+
+// is list empty?
+bool StringLinkedList::empty() const 
+    { return head == NULL; }
+
+// get front element
+const string &StringLinkedList::front() const 
+    { return head->elem; }
+
+// add to front of list
+void StringLinkedList::addFront(const string &e){
+    StringNode *v = new StringNode;   // create new node
+    v->elem = e;                     // store data
+    v->next = head;                  // head now follows v
+    head = v;                         // v is now the head
+}
+
+// remove front item
+void StringLinkedList::removeFront(){    
+    StringNode *old = head; // save current head
+    head = old->next;     // skip over old head
+    delete old;             // delete the old head
+}
+
+
+int main(){
+
+
+    return 0;
+}
+
+void StringLinkedList::print() const {
+    StringNode *curr = head;
+    while (curr != NULL) {
+        cout << curr->elem << " -> ";
+        curr = curr->next;
+    }
+    cout << "NULL" << endl;
+}
+
+
+// //=======================================================================
+// // Generic type singly linked list 
+
+// template <typename E>
+// class SNode { // singly linked list node
+// private:
+//     E elem;                      // linked list element value
+//     SNode<E> *next;              // next item in the list
+//     friend class SLinkedList<E>; // provide SLinkedList access
+// };
+
+// template <typename E>
+// class SLinkedList { // a singly linked list
+// public:
+//     SLinkedList();             // empty list constructor
+//     ~SLinkedList();            // destructor
+//     bool empty() const;        // is list empty?
+//     const E &front() const;    // return front element
+//     void addFront(const E &e); // add to front of list
+//     void removeFront();        // remove front item list
+// private:
+//     SNode<E> *head; // head of the list
+// };
+
+// template <typename E>
+// SLinkedList<E>::SLinkedList() // constructor
+//     : head(NULL){}
+
+// template <typename E>
+// bool SLinkedList<E>::empty() const // is list empty?
+//     { return head == NULL; }
+
+// template <typename E>
+// const E &SLinkedList<E>::front() const // return front element
+//     { return head->elem;}
+
+// template <typename E>
+// SLinkedList<E>::SLinkedList() // destructor
+//     { while (!empty()) removeFront(); }
+
+// template <typename E>
+// void SLinkedList<E>::addFront(const E &e) { // add to front of list                              
+//     SNode<E> *v = new SNode<E>; // create new node
+//     v->elem = e;                // store data
+//     v->next = head;            // head now follows v
+//     head = v;                   // v is now the head
+// }
+
+// template <typename E>
+// void SLinkedList<E>::removeFront(){ // remove front item                         
+//     SNode<E> *old = head; // save current head
+//     head = old->next;    // skip over old head
+//     delete old;           // delete the old head
+// }
