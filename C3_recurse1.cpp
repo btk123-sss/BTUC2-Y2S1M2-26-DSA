acc9099d9b0bf6c2b26ea3b75be03c55ff114cd6
btk123-sss <bunthern.kim9@gmail.com>
2026-09-02 08:57:36 +0700

    Auto-commit: 2026-09-02 08:57:36

--- C3_recurse1.cpp
+++ C3_recurse1.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 // Factorial function using Loop (iteration)
+// n! =  n*(n-1)*(n-2)* .. *1
+
 int f1(int n){
     if (n <= 0) return 1;
     else{
@@ -15,14 +17,36 @@ int f1(int n){
 }
 
 // Factorial function using recursion
+// n! =  n*(n-1)!
 int f2(int n){
     if (n == 0) return 1;
     else return n*f2(n-1);
 }
 
+void printLine(const string& s){
+    cout << s << endl;
+}
+
+void printNtime(int n, const string& s){
+    for (int i=0; i< n; i++){
+        printLine(s);
+    }
+}
+
+void RecursionPrintNtime(int n, const string& s){
+    cout << s << endl;
+    if(n==1){
+        return;
+    }
+    RecursionPrintNtime(n-1, s);
+}
+
 int main () {
 
+    printNtime(5, "Hello");
+    RecursionPrintNtime(6, "World!");
 
+    cout << f2(5) << endl;
 
 
 
