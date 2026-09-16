3ee78561a417a14c304b41828e77023541bab7e2
btk123-sss <bunthern.kim9@gmail.com>
2026-09-16 09:38:04 +0700

    Auto-commit: 2026-09-16 09:38:04

--- C3_recurse3.cpp
+++ C3_recurse3.cpp
@@ -19,13 +19,39 @@ void print_all_strings(int n, const vector<char> char_set, string& word){
     }
 }
 
+void print_all_strings_nonrepeat(int n, const vector<char> char_set, 
+    string &word, vector<bool>& used)  {
+    // base case
+    if (word.length() == n)
+    {
+        cout << word << "\n";
+        return;
+    }
+    // recursion with backtracking
+    for (int i = 0; i < char_set.size(); i++)
+    {
+        if(used[i] == false){
+            used[i] = true;
+            word.push_back(char_set[i]);
+            print_all_strings_nonrepeat(n, char_set, word, used);
+            word.pop_back();
+            used[i] = false;
+        }
+    }
+}
+
+
+
+
 int main()
 {
     vector<char> K = {'1', '2', '3'};
     int n = 3;
     string word;
+    vector<bool> used(K.size(), false);
 
-    print_all_strings(n, K, word);
+    // print_all_strings(n, K, word);
+    print_all_strings_nonrepeat(n, K, word, used);
 
     return 0;
 }
